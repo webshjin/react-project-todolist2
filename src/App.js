5780711a276076919f84445e4dfd8789d8b30248
unknown <web.shjin@gmail.com>
2023-10-11 13:22:10 +0900

    mockup data 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
 import TodoList from './components/TodoList';
 
+const mockupTodos = [
+  {
+    id: 0,
+    isDone: false,
+    content: '빨래하기',
+    createdDate: new Date().getTime(),
+  },
+
+  {
+    id: 1,
+    isDone: true,
+    content: '수업하기',
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  const [todos, setTodos] = useState(mockupTodos);
   return (
     <div className="App">
       <div>
