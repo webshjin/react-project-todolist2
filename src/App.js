fefe5ea0f53c98fe60854e0cd08791d8bc6e5251
unknown <web.shjin@gmail.com>
2023-10-12 15:52:11 +0900

    useReducer로 상태 로직 분리

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useReducer, useRef, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import TodoEditor from './components/TodoEditor';
@@ -20,39 +20,63 @@ const mockupTodos = [
   },
 ];
 
-function App() {
-  const [todos, setTodos] = useState(mockupTodos);
-  const idRef = useRef(2);
+// state : 상태변화될데이터(State), 여기에서는 todos
+// action : State를 어떻게 변화 시킬 것이냐(dispatch()로부터 넘겨져온 매개변수(객체)를 받음)
+function reducer(state, action) {
+  switch (action.type) {
+    case 'create': {
+      return [action.newTodo, ...state];
+    }
+    case 'update': {
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, isDone: !todo.isDone } : todo,
+      );
+    }
+    case 'delete': {
+      return state.filter((todo) => todo.id !== action.id);
+    }
 
-  console.log(idRef);
+    default:
+      return state;
+  }
+}
+
+function App() {
+  // const [todos, setTodos] = useState(mockupTodos); // State 훅으로 상태 관리
+
+  // todos라는 데이터가 상태 변화 될 때 reducer 함수로 변화하는 로직을 관리 하겠다.
+  // todos라는 상태는 초기값으로 mockupTodos를 가지게 된다.
+  const [todos, dispatch] = useReducer(reducer, mockupTodos);
+
+  const idRef = useRef(2);
 
   const onCreate = (content) => {
     // 입력받은 content를 멤버로 하는 새로운 할일 객체 생성
-    const newTodo = {
-      id: idRef.current,
-      content: content,
-      isDone: false,
-      createdDate: new Date().getTime(),
-    };
-    setTodos([newTodo, ...todos]); // 스프레드 연산자를 통해 불변성을 지키며 새로운 배열 setTodos 할당
+    dispatch({
+      type: 'create',
+      newTodo: {
+        id: idRef.current,
+        content: content,
+        isDone: false,
+        createdDate: new Date().getTime(),
+      },
+    });
+
     idRef.current += 1;
   };
 
   const onUpdate = (id) => {
-    // console.log('수정 작업 해야 함...');
-
-    // 넘겨져온 수정된 TodoItem.id 와 기존 todos배열 안에 있는 요소의 id값과 하여
-    // 같다면(수정된 할일) isDone 속성의 값을 기존 isDone 속성의 값에서 !(반전)  시켜 새로운 객체로 반환하고, 반환된 새로운 객체를 map()를 이용함으로써 새로운 배열을 결국에 setTodos()에 넣어주게된다.
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo,
-      ),
-    );
+    dispatch({
+      type: 'update',
+      id: id,
+    });
   };
 
   const onDelete = (id) => {
-    // console.log(`${id}번의 할일을 삭제합니다~!`);
-    setTodos(todos.filter((todo) => todo.id !== id));
+    dispatch({
+      type: 'delete',
+      id: id,
+    });
   };
 
   return (
